8caf1fd92ebd0dbbd7b989543d212bee7161ab9f
crisine <crisine@knou.ac.kr>
2024-02-16 06:13:40 +0900

    feat: 전체 할 일 목록을 조회할 수 있는 기능 추가

--- SeSAC4TodoList/Todo/TodoViewController.swift
+++ SeSAC4TodoList/Todo/TodoViewController.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TodoViewController: BaseViewController {
 
     let todoTableView = UITableView()
+    var allToDoList: [TodoModel] = [] {
+        didSet {
+            todoTableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +23,11 @@ class TodoViewController: BaseViewController {
         todoTableView.delegate = self
         todoTableView.dataSource = self
         todoTableView.register(TodoTableViewCell.self, forCellReuseIdentifier: "TodoTableViewCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let realm = try! Realm()
+        allToDoList = realm.objects(TodoModel.self).map { $0 }
     }
 
     override func configureHierarchy() {
@@ -38,19 +49,22 @@ class TodoViewController: BaseViewController {
 extension TodoViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return allToDoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoTableViewCell", for: indexPath) as! TodoTableViewCell
+        let index = indexPath.row
         
         let dateformatter = DateFormatter()
         dateformatter.dateFormat = "yyyy. MM. dd."
         
-        cell.titleLabel.text = "임시 제목 테스트입니다."
+        cell.titleLabel.text = allToDoList[index].title
         
-        // TODO: dueDate가 금일이라면 '오늘' 로 표시 후, 시간을 표시
-        cell.dateLabel.text = dateformatter.string(from: Date())
+        if let dueDate = allToDoList[index].dueDate {
+            // TODO: dueDate가 금일이라면 '오늘' 로 표시 후, 시간을 표시
+            cell.dateLabel.text = dateformatter.string(from: dueDate)
+        }
         
         return cell
     }
